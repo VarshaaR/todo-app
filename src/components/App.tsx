e7a3b478d68d450459cead1def17cf4aea51ce59
VarshaaR <varshaa93@gmail.com>
2025-04-16 19:23:15 +0530

    chore: added material ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/main.scss";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <>
       <h1 className="bg-blue-500 text-white p-4 rounded-lg">Todo List</h1>
+      <Button variant="outlined">Outlined</Button>
     </>
   );
 }
