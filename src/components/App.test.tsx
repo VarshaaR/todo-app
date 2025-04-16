d39ef177e23b213609c3c7d40bf458b390201016
VarshaaR <varshaa93@gmail.com>
2025-04-16 14:27:52 +0530

    chore: added tailwindcss and scss support

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Vite + React heading", () => {
+test("renders Todo List heading", () => {
   render(<App />);
-  const headingElement = screen.getByText(/vite \+ react/i);
+  const headingElement = screen.getByText(/todo list/i);
   expect(headingElement).toBeInTheDocument();
 });
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/main.scss";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 text-white p-4 rounded-lg">Todo List</h1>
+    </>
+  );
+}
+
+export default App;
